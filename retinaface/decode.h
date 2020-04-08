35d59c91b3815d19f1454e6eae42bfb22821575a
wang-xinyu <wangxinyu_es@163.com>
2020-04-08 14:42:02 +0000

    retinaface ssh, decode plugin

---
+++ retinaface/decode.h
@@ -0,0 +1,61 @@
+#ifndef _DECODE_CU_H
+#define _DECODE_CU_H
+
+#include "NvInfer.h"
+
+namespace decodeplugin
+{
+    struct alignas(float) Detection{
+        //x y w h
+        float bbox[4];
+        float class_confidence;
+        float landmark[10];
+    };
+}
+
+
+namespace nvinfer1
+{
+    class DecodePlugin: public IPluginExt
+    {
+    public:
+        explicit DecodePlugin(const int cudaThread = 256);
+        DecodePlugin(const void* data, size_t length);
+
+        ~DecodePlugin();
+
+        int getNbOutputs() const override
+        {
+            return 1;
+        }
+
+        Dims getOutputDimensions(int index, const Dims* inputs, int nbInputDims) override;
+
+        bool supportsFormat(DataType type, PluginFormat format) const override { 
+            return type == DataType::kFLOAT && format == PluginFormat::kNCHW; 
+        }
+
+        void configureWithFormat(const Dims* inputDims, int nbInputs, const Dims* outputDims, int nbOutputs, DataType type, PluginFormat format, int maxBatchSize) override {};
+
+        int initialize() override;
+
+        virtual void terminate() override {};
+
+        virtual size_t getWorkspaceSize(int maxBatchSize) const override { return 0;}
+
+        virtual int enqueue(int batchSize, const void*const * inputs, void** outputs, void* workspace, cudaStream_t stream) override;
+
+        virtual size_t getSerializationSize() override;
+
+        virtual void serialize(void* buffer) override;
+
+        void forwardGpu(const float *const * inputs,float * output, cudaStream_t stream,int batchSize = 1);
+
+    private:
+        const int input_h_ = 384;
+        const int input_w_ = 640;
+        int thread_count_ = 256;
+    };
+};
+
+#endif 
