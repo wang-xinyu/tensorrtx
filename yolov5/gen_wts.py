5b90354d289528e298d37b29759f55306e018c8c
wang-xinyu <wangxinyu_es@163.com>
2021-04-07 05:21:37 +0000

    fix genwts file close

--- yolov5/gen_wts.py
+++ yolov5/gen_wts.py
@@ -8,12 +8,12 @@ device = select_device('cpu')
 model = torch.load('weights/yolov5s.pt', map_location=device)['model'].float()  # load to FP32
 model.to(device).eval()
 
-f = open('yolov5s.wts', 'w')
-f.write('{}\n'.format(len(model.state_dict().keys())))
-for k, v in model.state_dict().items():
-    vr = v.reshape(-1).cpu().numpy()
-    f.write('{} {} '.format(k, len(vr)))
-    for vv in vr:
-        f.write(' ')
-        f.write(struct.pack('>f',float(vv)).hex())
-    f.write('\n')
+with open('yolov5s.wts', 'w') as f:
+    f.write('{}\n'.format(len(model.state_dict().keys())))
+    for k, v in model.state_dict().items():
+        vr = v.reshape(-1).cpu().numpy()
+        f.write('{} {} '.format(k, len(vr)))
+        for vv in vr:
+            f.write(' ')
+            f.write(struct.pack('>f',float(vv)).hex())
+        f.write('\n')
