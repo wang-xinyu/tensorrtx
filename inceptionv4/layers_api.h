7d7911e488c2a550527618cfc827d94f6567dfba
makaveli10 <suryanvineet47@gmail.com>
2021-04-18 14:03:38 +0530

    define inception layers

--- inceptionv4/layers_api.h
+++ inceptionv4/layers_api.h
@@ -13,6 +13,14 @@ using namespace nvinfer1;
 namespace trtxlayers {
 
     // Declare your layers here
+    IScaleLayer* addBatchNorm2d(
+        INetworkDefinition *network, 
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input, 
+        std::string lname, 
+        float eps
+    )
+
     IActivationLayer* basicConv2d(
         INetworkDefinition *network,
         std::map<std::string, Weights>& weightMap, 
@@ -21,7 +29,64 @@ namespace trtxlayers {
         DimsHW ksize, 
         int s, 
         DimsHW p, 
-        std::string lname)
+        std::string lname
+    )
+
+    IConcatenationLayer* mixed_3a(
+        INetworkDefinition *network,
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input,  
+        std::string lname
+    )
+
+    IConcatenationLayer* mixed_4a(
+        INetworkDefinition *network,
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input,  
+        std::string lname
+    )
+
+    IConcatenationLayer* mixed_5a(
+        INetworkDefinition *network,
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input,  
+        std::string lname
+    )
+
+    IConcatenationLayer* inceptionA(
+        INetworkDefinition *network,
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input,  
+        std::string lname
+    )
+
+    IConcatenationLayer* reductionA(
+        INetworkDefinition *network,
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input,  
+        std::string lname
+    )
+    
+    IConcatenationLayer* inceptionB(
+        INetworkDefinition *network,
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input,  
+        std::string lname
+    )
+
+    IConcatenationLayer* reductionB(
+        INetworkDefinition *network,
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input,  
+        std::string lname
+    )
+
+    IConcatenationLayer* inceptionC(
+        INetworkDefinition *network,
+        std::map<std::string, Weights>& weightMap, 
+        ITensor& input,  
+        std::string lname
+    )
 }
 
 #endif  // TRTX_LAYERS_API_H
