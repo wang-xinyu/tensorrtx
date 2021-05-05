713d61e1c51194c643fa211dc6b87919ecd190a4
makaveli10 <suryanvineet47@gmail.com>
2021-05-05 15:19:20 +0530

    add: yolov4-csp net def

--- scaled-yolov4/mish.cu
+++ scaled-yolov4/mish.cu
@@ -4,7 +4,6 @@
 #include <iostream>
 #include "mish.h"
 
-
 namespace nvinfer1
 {
     MishPlugin::MishPlugin()
@@ -18,7 +17,7 @@ namespace nvinfer1
     // create the plugin at runtime from a byte stream
     MishPlugin::MishPlugin(const void* data, size_t length)
     {
-        assert(length==sizeof(input_size_));
+        assert(length == sizeof(input_size_));
         input_size_ = *reinterpret_cast<const int*>(data);
     }
 
@@ -28,7 +27,7 @@ namespace nvinfer1
     }
 
     size_t MishPlugin::getSerializationSize() const
-    {
+    {  
         return sizeof(input_size_);
     }
 
@@ -39,16 +38,14 @@ namespace nvinfer1
 
     Dims MishPlugin::getOutputDimensions(int index, const Dims* inputs, int nbInputDims)
     {
-        assert(nbInputDims==1);
-        assert(index==0);
-
+        assert(nbInputDims == 1);
+        assert(index == 0);
         input_size_ = inputs[0].d[0] * inputs[0].d[1] * inputs[0].d[2];
-
-        // output dimensions
+        // Output dimensions
         return Dims3(inputs[0].d[0], inputs[0].d[1], inputs[0].d[2]);
     }
 
-    // set plugin namespace
+    // Set plugin namespace
     void MishPlugin::setPluginNamespace(const char* pluginNamespace)
     {
         mPluginNamespace = pluginNamespace;
@@ -113,40 +110,32 @@ namespace nvinfer1
         return p;
     }
 
-    // tanh helper kernel
-    __device__ float tanh_activate_kernel(float x)
-    {
-        return (2/(1 + expf(-2*x)) - 1);
-    }
+    __device__ float tanh_activate_kernel(float x){return (2/(1 + expf(-2*x)) - 1);}
 
-    // softplus kernel
-    __device__ float softplus_kernel(float x, float threshold = 20)
-    {
-        if (x > threshold) return x;                  // too large
-        else if (x < -threshold) return expf(x);      // too small
+    __device__ float softplus_kernel(float x, float threshold = 20) {
+        if (x > threshold) return x;                // too large
+        else if (x < -threshold) return expf(x);    // too small
         return logf(expf(x) + 1);
     }
 
-    __global__ void mish_kernel(const float* input, float* output, int n)
-    {
-        //int i = (blockIdx.x + blockIdx.y * gridDim.x) * blockDim.x + threadIdx.x;
-        int i = threadIdx.x + blockDim.x * blockIdx.x;
+    __global__ void mish_kernel(const float *input, float *output, int num_elem) {
 
-        if (i < n) {
-            //float t = exp(input[i]);
-            //if (input[i] > 20.0) {
-            //    t *= t;
-            //    output[i] = (t - 1.0) / (t + 1.0);
-            //} else {
-            //    float tt = t * t;
-            //    output[i] = (tt + 2.0 * t) / (tt + 2.0 * t + 2.0);
-            //}
-            output[i] = input[i] * tanh_activate_kernel( softplus_kernel(input[i]));
-        }
+        int idx = threadIdx.x + blockDim.x * blockIdx.x;
+        if (idx >= num_elem) return;
+
+        //float t = exp(input[idx]);
+        //if (input[idx] > 20.0) {
+        //    t *= t;
+        //    output[idx] = (t - 1.0) / (t + 1.0);
+        //} else {
+        //    float tt = t * t;
+        //    output[idx] = (tt + 2.0 * t) / (tt + 2.0 * t + 2.0);
+        //}
+        //output[idx] *= input[idx];
+        output[idx] = input[idx] * tanh_activate_kernel(softplus_kernel(input[idx]));
     }
 
-    void MishPlugin::forwardGpu(const float *const * inputs, float* output, cudaStream_t stream, int batchSize)
-    {
+    void MishPlugin::forwardGpu(const float *const * inputs, float* output, cudaStream_t stream, int batchSize) {
         int block_size = thread_count_;
         int grid_size = (input_size_ * batchSize + block_size - 1) / block_size;
         mish_kernel<<<grid_size, block_size>>>(inputs[0], output, input_size_ * batchSize);
@@ -202,4 +191,6 @@ namespace nvinfer1
         obj->setPluginNamespace(mNamespace.c_str());
         return obj;
     }
+
 }
+
--- scaled-yolov4/yololayer.h
+++ scaled-yolov4/yololayer.h
@@ -11,8 +11,8 @@ namespace Yolo
     static constexpr float IGNORE_THRESH = 0.1f;
     static constexpr int MAX_OUTPUT_BBOX_COUNT = 1000;
     static constexpr int CLASS_NUM = 80;
-    static constexpr int INPUT_H = 608;
-    static constexpr int INPUT_W = 608;
+    static constexpr int INPUT_H = 512;
+    static constexpr int INPUT_W = 512;
 
     struct YoloKernel
     {
