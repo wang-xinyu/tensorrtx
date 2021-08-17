76078d8c660c17b2225fe95e888e6432b57e56d1
CristiFati <fati_utcluj@yahoo.com>
2021-08-17 21:52:30 +0300

    Improve output (wts) file name generation

--- yolov3/gen_wts.py
+++ yolov3/gen_wts.py
@@ -1,18 +1,20 @@
 import struct
 import sys
+import os
 from models import *
 from utils.utils import *
 
 model = Darknet('cfg/yolov3.cfg', (608, 608))
-weights = sys.argv[1]
+weights_file = sys.argv[1]
 device = torch_utils.select_device('0')
-if weights.endswith('.pt'):  # pytorch format
-    model.load_state_dict(torch.load(weights, map_location=device)['model'])
+if weights_file.endswith('.pt'):  # pytorch format
+    model.load_state_dict(torch.load(weights_file, map_location=device)['model'])
 else:  # darknet format
-    load_darknet_weights(model, weights)
+    load_darknet_weights(model, weights_file)
 model = model.eval()
+wts_file = sys.argv[2] if len(sys.argv) > 2 else os.path.splitext(weights_file)[0] + '.wts'
 
-with open('yolov3.wts', 'w') as f:
+with open(wts_file, 'w') as f:
     f.write('{}\n'.format(len(model.state_dict().keys())))
     for k, v in model.state_dict().items():
         vr = v.reshape(-1).cpu().numpy()
