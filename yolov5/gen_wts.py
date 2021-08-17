3c2fc39efb78f7a7857e0eff2846fcae914557c0
CristiFati <fati_utcluj@yahoo.com>
2021-08-17 23:58:57 +0300

    More on out file name

--- yolov5/gen_wts.py
+++ yolov5/gen_wts.py
@@ -4,13 +4,24 @@ import sys
 import os
 from utils.torch_utils import select_device
 
+
 # Initialize
 device = select_device('cpu')
 pt_file = sys.argv[1]
 # Load model
 model = torch.load(pt_file, map_location=device)['model'].float()  # load to FP32
 model.to(device).eval()
-wts_file = sys.argv[2] if len(sys.argv) > 2 else os.path.splitext(pt_file)[0] + '.wts'
+
+if len(sys.argv) > 2:
+    if os.path.isdir(sys.argv[2]):
+        wts_file = os.path.join(
+            sys.argv[2],
+            os.path.splitext(os.path.basename(pt_file))[0] + '.wts')
+    else:
+        wts_file = sys.argv[2]
+else:
+    wts_file = os.path.splitext(pt_file)[0] + '.wts'
+
 
 with open(wts_file, 'w') as f:
     f.write('{}\n'.format(len(model.state_dict().keys())))
