09625f1ce5d5e95b5b57d409dcd532146223a5aa
CristiFati <fati_utcluj@yahoo.com>
2021-08-17 21:38:45 +0300

    Improve output (wts) file name generation

--- yolov5/gen_wts.py
+++ yolov5/gen_wts.py
@@ -1,6 +1,7 @@
 import torch
 import struct
 import sys
+import os
 from utils.torch_utils import select_device
 
 # Initialize
@@ -9,8 +10,9 @@ pt_file = sys.argv[1]
 # Load model
 model = torch.load(pt_file, map_location=device)['model'].float()  # load to FP32
 model.to(device).eval()
+wts_file = sys.argv[2] if len(sys.argv) > 2 else os.path.splitext(pt_file)[0] + '.wts'
 
-with open(pt_file.split('.')[0] + '.wts', 'w') as f:
+with open(wts_file, 'w') as f:
     f.write('{}\n'.format(len(model.state_dict().keys())))
     for k, v in model.state_dict().items():
         vr = v.reshape(-1).cpu().numpy()
