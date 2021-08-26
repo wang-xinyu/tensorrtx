e1b36e6649f1db577943b690ad489fa275bfd411
CristiFati <fati_utcluj@yahoo.com>
2021-08-26 23:55:18 +0300

    Improve output (wts) file name generation

--- yolov5/gen_wts.py
+++ yolov5/gen_wts.py
@@ -1,21 +1,41 @@
-import torch
-import struct
 import sys
+import argparse
+import os
+import struct
+import torch
 from utils.torch_utils import select_device
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Convert .pt file to .wts')
+    parser.add_argument('pt_file', help='Input (.pt) file (required)')
+    parser.add_argument('wts_file', nargs="?", help='Output (.wts) file path (optional)')
+    args = parser.parse_args()
+    if not os.path.isfile(args.pt_file):
+        raise SystemExit('Invalid input file')
+    if args.wts_file is None:
+        args.wts_file = os.path.splitext(args.pt_file)[0] + '.wts'
+    elif os.path.isdir(args.wts_file):
+        args.wts_file = os.path.join(
+            args.wts_file,
+            os.path.splitext(os.path.basename(args.pt_file))[0] + '.wts')
+    return args.pt_file, args.wts_file
+
+
+pt_file, wts_file = parse_args()
 
 # Initialize
 device = select_device('cpu')
-pt_file = sys.argv[1]
 # Load model
 model = torch.load(pt_file, map_location=device)['model'].float()  # load to FP32
 model.to(device).eval()
 
-with open(pt_file.split('.')[0] + '.wts', 'w') as f:
+with open(wts_file, 'w') as f:
     f.write('{}\n'.format(len(model.state_dict().keys())))
     for k, v in model.state_dict().items():
         vr = v.reshape(-1).cpu().numpy()
         f.write('{} {} '.format(k, len(vr)))
         for vv in vr:
             f.write(' ')
-            f.write(struct.pack('>f',float(vv)).hex())
+            f.write(struct.pack('>f' ,float(vv)).hex())
         f.write('\n')
