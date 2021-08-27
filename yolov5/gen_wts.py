b547946270163e9ec0d8be711d428743e7bb8da2
CristiFati <fati_utcluj@yahoo.com>
2021-08-27 18:54:16 +0300

    Named arguments

--- yolov5/gen_wts.py
+++ yolov5/gen_wts.py
@@ -8,18 +8,18 @@ from utils.torch_utils import select_device
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Convert .pt file to .wts')
-    parser.add_argument('pt_file', help='Input (.pt) file (required)')
-    parser.add_argument('wts_file', nargs="?", help='Output (.wts) file path (optional)')
+    parser.add_argument('-w', '--weights', required=True, help='Input weights (.pt) file path (required)')
+    parser.add_argument('-o', '--output', help='Output (.wts) file path (optional)')
     args = parser.parse_args()
-    if not os.path.isfile(args.pt_file):
+    if not os.path.isfile(args.weights):
         raise SystemExit('Invalid input file')
-    if args.wts_file is None:
-        args.wts_file = os.path.splitext(args.pt_file)[0] + '.wts'
-    elif os.path.isdir(args.wts_file):
-        args.wts_file = os.path.join(
-            args.wts_file,
-            os.path.splitext(os.path.basename(args.pt_file))[0] + '.wts')
-    return args.pt_file, args.wts_file
+    if args.output is None:
+        args.output = os.path.splitext(args.weights)[0] + '.wts'
+    elif os.path.isdir(args.output):
+        args.output = os.path.join(
+            args.output,
+            os.path.splitext(os.path.basename(args.weights))[0] + '.wts')
+    return args.weights, args.output
 
 
 pt_file, wts_file = parse_args()
