1d82ff03ed534126402803c7f2d5d7caba57d286
liuqi <46275888+liuqi123123@users.noreply.github.com>
2021-10-19 15:03:55 +0800

    Update preprocess.h

--- yolov5/preprocess.h
+++ yolov5/preprocess.h
@@ -18,5 +18,5 @@ struct AffineMatrix{
 
 void preprocess_kernel_img(uint8_t* src, int src_width, int src_height, 
         float* dst, int dst_width, int dst_height,
-		cudaStream_t stream);
+	cudaStream_t stream);
 #endif  // PREPROCESS_H
