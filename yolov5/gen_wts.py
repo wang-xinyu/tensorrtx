1aa11f074792bb7c28d86ca2920247a1ef564e06
Mr.Li <1055271769@qq.com>
2021-10-21 17:10:36 +0800

    Fix yolov5-v6  error
    Modified content:
          When the anchor_grid layer is missing, the layer will be inserted at the corresponding position, and the total number of layers will be +1.
    
    Features:
    - No change in usage, no need for official weight comparison.
    - Support yolov5-v6 series. such as yolov5 n/s/m/l/x  ann n6/s6/m6/l6/x6

--- yolov5/gen_wts.py
+++ yolov5/gen_wts.py
@@ -4,6 +4,36 @@ import os
 import struct
 import torch
 from utils.torch_utils import select_device
+import numpy as np
+
+anchor_grid={
+    'model.24.anchor_grid':[
+        [10,13, 16,30, 33,23],      # P3/8
+        [30,61, 62,45, 59,119],     # P4/16
+        [116,90, 156,198, 373,326]  # P5/32
+    ],
+    'model.33.anchor_grid':[
+        [19,27,  44,40,  38,94],       # P3/8
+        [96,68,  86,152,  180,137],    # P4/16
+        [140,301,  303,264,  238,542], # P5/32
+        [436,615,  739,380,  925,792]  # P6/64
+    ]
+}
+
+def verify_model(model):
+    '''
+    Verify that the anchor_grid layer exists
+    '''
+    module_key=None
+    model_keys=model.state_dict().keys()
+    if not("model.24.anchor_grid" in  model_keys  or "model.33.anchor_grid" in  model_keys):
+        if "model.24.anchors" in  model_keys:
+            module_key="model.24.anchor_grid"
+        elif "model.33.anchors" in  model_keys:
+            module_key="model.33.anchor_grid"
+        else:
+            raise NotImplementedError
+    return  module_key
 
 
 def parse_args():
@@ -29,9 +59,10 @@ device = select_device('cpu')
 # Load model
 model = torch.load(pt_file, map_location=device)['model'].float()  # load to FP32
 model.to(device).eval()
+module_key=verify_model(model)
 
 with open(wts_file, 'w') as f:
-    f.write('{}\n'.format(len(model.state_dict().keys())))
+    f.write('{}\n'.format(len(model.state_dict().keys()) if not module_key else  len(model.state_dict().keys())+1))
     for k, v in model.state_dict().items():
         vr = v.reshape(-1).cpu().numpy()
         f.write('{} {} '.format(k, len(vr)))
@@ -39,3 +70,14 @@ with open(wts_file, 'w') as f:
             f.write(' ')
             f.write(struct.pack('>f' ,float(vv)).hex())
         f.write('\n')
+
+        # insert anchor_grid
+        if module_key and 'anchors' in  k:
+            k=module_key
+            vr=anchor_grid[k]
+            vr=np.array(vr).flatten()
+            f.write('{} {} '.format(k, len(vr)))
+            for vv in vr:
+                f.write(' ')
+                f.write(struct.pack('>f' ,float(vv)).hex())
+            f.write('\n')
