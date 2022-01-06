a0d575c94ea61565ab5f2d618e622d87bd664033
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2022-01-06 12:42:15 -0500

    change ref from yolov5 to v3

--- yolov3/yolov3_trt.py
+++ yolov3/yolov3_trt.py
@@ -68,7 +68,7 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
         )
 
 
-class YoLov5TRT(object):
+class YoLov3TRT(object):
     """
     description: A YOLOv5 class that warps TensorRT ops, preprocess and postprocess ops.
     """
@@ -388,13 +388,13 @@ class YoLov5TRT(object):
 
 
 class inferThread(threading.Thread):
-    def __init__(self, yolov5_wrapper, image_path_batch):
+    def __init__(self, yolov3_wrapper, image_path_batch):
         threading.Thread.__init__(self)
-        self.yolov5_wrapper = yolov5_wrapper
+        self.yolov3_wrapper = yolov3_wrapper
         self.image_path_batch = image_path_batch
 
     def run(self):
-        batch_image_raw, use_time = self.yolov5_wrapper.infer(self.yolov5_wrapper.get_raw_image(self.image_path_batch))
+        batch_image_raw, use_time = self.yolov3_wrapper.infer(self.yolov3_wrapper.get_raw_image(self.image_path_batch))
         for i, img_path in enumerate(self.image_path_batch):
             parent, filename = os.path.split(img_path)
             save_name = os.path.join('output', filename)
@@ -404,12 +404,12 @@ class inferThread(threading.Thread):
 
 
 class warmUpThread(threading.Thread):
-    def __init__(self, yolov5_wrapper):
+    def __init__(self, yolov3_wrapper):
         threading.Thread.__init__(self)
-        self.yolov5_wrapper = yolov5_wrapper
+        self.yolov3_wrapper = yolov3_wrapper
 
     def run(self):
-        batch_image_raw, use_time = self.yolov5_wrapper.infer(self.yolov5_wrapper.get_raw_image_zeros())
+        batch_image_raw, use_time = self.yolov3_wrapper.infer(self.yolov3_wrapper.get_raw_image_zeros())
         print('warm_up->{}, time->{:.2f}ms'.format(batch_image_raw[0].shape, use_time * 1000))
 
 
@@ -442,23 +442,23 @@ if __name__ == "__main__":
         shutil.rmtree('output/')
     os.makedirs('output/')
     # a YoLov5TRT instance
-    yolov5_wrapper = YoLov5TRT(engine_file_path)
+    yolov3_wrapper = YoLov3TRT(engine_file_path)
     try:
-        print('batch size is', yolov5_wrapper.batch_size)
+        print('batch size is', yolov3_wrapper.batch_size)
         
         image_dir = "samples/"
-        image_path_batches = get_img_path_batches(yolov5_wrapper.batch_size, image_dir)
+        image_path_batches = get_img_path_batches(yolov3_wrapper.batch_size, image_dir)
 
         for i in range(10):
             # create a new thread to do warm_up
-            thread1 = warmUpThread(yolov5_wrapper)
+            thread1 = warmUpThread(yolov3_wrapper)
             thread1.start()
             thread1.join()
         for batch in image_path_batches:
             # create a new thread to do inference
-            thread1 = inferThread(yolov5_wrapper, batch)
+            thread1 = inferThread(yolov3_wrapper, batch)
             thread1.start()
             thread1.join()
     finally:
         # destroy the instance
-        yolov5_wrapper.destroy()
+        yolov3_wrapper.destroy()
