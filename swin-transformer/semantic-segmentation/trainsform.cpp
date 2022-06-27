dbdad6846c7d775d2428e54a7c6161277bd6fcb9
Egor Tolmachev <t333ga@gmail.com>
2022-06-27 08:51:24 +0300

    Fix comparison with args in mode check. (#1019)
    @wang-xinyu @wdhao  Before we had assignment, not comparison.(somebody forgot "=" but not only)

--- swin-transformer/semantic-segmentation/trainsform.cpp
+++ swin-transformer/semantic-segmentation/trainsform.cpp
@@ -225,7 +225,8 @@ int main(int argc, char** argv)
     cout << "begin" << endl;
     //string wts = "G:/shaj/trainsform/ktn5n6_29.511.21.8.wts";
     //string eng = "G:/shaj/trainsform/trainsform.eng";
-    if (argv[1] = "-s") {
+    std::string argv1 = argv[1];
+    if (argv1 == "-s") {
         string wts = argv[2];
         string eng = argv[3];
         createEng(wts,eng);
