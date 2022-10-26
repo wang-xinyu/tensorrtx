7a6b62694225655eed0265fde45b4fbf7d54f4b8
jinlj <jincsu@126.com>
2022-10-26 15:04:27 +0800

    Feature: add modified yolop tensorrt implementation (#1129)
    * add simplified Logger class
    
    * add modified yolop tensorrtx implementation
    
    * add gen_wts.py
    
    * add CMakeLists.txt
    
    * add yolop tensorrtx README.md
    
    * fix yolop spelling
    
    * simplify yolop code
    
    * fix yolop README.md
    
    Co-authored-by: aliceint(zhanglj) <2464313547@qq.com>

---
+++ yolop/README.md
@@ -0,0 +1,82 @@
+YoloP TensorRT Usage
+=====
+
+Fork from [hustvl/YOLOP](https://github.com/hustvl/YOLOP), modify from [ausk/YOLOP](https://github.com/ausk/YOLOP/), push into [wang-xinyu/tensorrtx](https://github.com/wang-xinyu/tensorrtxls)
+
+
+## 1. Prepare building environments
+
+Make sure you have install `c++`(support c++11)、 `cmake`、`opencv`(4.x)、`cuda`(10.x)、`nvinfer`(7.x).
+
+
+## 2. build yolop
+
+Go to `yolop`.
+
+```
+mkdir build
+cd build
+
+cmake ..
+make
+```
+
+Now you can get `yolop` and `libmyplugins.so`.
+
+
+## 3. Generate and test trt
+
+Go to `yolop/build`.
+
+### 3.1 generate yolop.wts
+Download/Clone [YOLOP](https://github.com/hustvl/YOLOP)
+
+Edit `gen_wts.py` , change `YOLOP_BASE_DIR` to realpath of `YOLOP`.
+
+```
+# [WARN] Please download/clone YOLOP, then set YOLOP_BASE_DIR to the root of YOLOP
+python3 ../gen_wts.py
+```
+
+### 3.2 generate yolop.trt
+```
+./yolop -s yolop.wts  yolop.trt
+```
+
+Now you have such files:  `libmyplugins.so yolop yolop.wts  yolop.trt`
+
+
+### 3.3 test yolop.trt
+```
+mkdir ../results
+
+YOLOP_BASE_DIR=/home/user/jetson/tmp/YOLOP
+./yolop -d yolop.trt  $YOLOP_BASE_DIR/inference/images/
+```
+
+It will output like as follow if successful! ( test on `Jetson Xavier NX - Jetpack 4.4`)
+```
+1601ms # the fist time is slow
+26ms   # then it is faster
+29ms
+27ms
+29ms
+29ms
+```
+
+![](https://user-images.githubusercontent.com/4545060/197756635-38348dc5-d8e7-4ae3-be56-6b231dd2f5db.jpg)
+
+
+
+----------------------------------------
+
+
+```BibTeX
+@misc{2108.11250,
+Author = {Dong Wu and Manwen Liao and Weitian Zhang and Xinggang Wang},
+Title = {YOLOP: You Only Look Once for Panoptic Driving Perception},
+Year = {2021},
+Eprint = {arXiv:2108.11250},
+}
+```
+
---
+++ yolop/yololayer.h
@@ -0,0 +1,139 @@
+#ifndef _YOLO_LAYER_H
+#define _YOLO_LAYER_H
+
+#include <vector>
+#include <string>
+#include "NvInfer.h"
+
+namespace Yolo
+{
+    static constexpr int CHECK_COUNT = 3;
+    static constexpr float IGNORE_THRESH = 0.1f;
+    struct YoloKernel
+    {
+        int width;
+        int height;
+        float anchors[CHECK_COUNT * 2];
+    };
+    static constexpr int MAX_OUTPUT_BBOX_COUNT = 1000;
+    static constexpr int CLASS_NUM = 1;
+    static constexpr int INPUT_H = 384;
+    static constexpr int INPUT_W = 640;
+    static constexpr int IMG_H = 360;
+    static constexpr int IMG_W = 640;
+
+    static constexpr int LOCATIONS = 4;
+    struct alignas(float) Detection {
+        //center_x center_y w h
+        float bbox[LOCATIONS];
+        float conf;  // bbox_conf * cls_conf
+        float class_id;
+    };
+}
+
+namespace nvinfer1
+{
+    class YoloLayerPlugin : public IPluginV2IOExt
+    {
+    public:
+        YoloLayerPlugin(int classCount, int netWidth, int netHeight, int maxOut, const std::vector<Yolo::YoloKernel>& vYoloKernel);
+        YoloLayerPlugin(const void* data, size_t length);
+        ~YoloLayerPlugin();
+
+        int getNbOutputs() const override
+        {
+            return 1;
+        }
+
+        Dims getOutputDimensions(int index, const Dims* inputs, int nbInputDims) override;
+
+        int initialize() override;
+
+        virtual void terminate() override {};
+
+        virtual size_t getWorkspaceSize(int maxBatchSize) const override { return 0; }
+
+        virtual int enqueue(int batchSize, const void*const * inputs, void** outputs, void* workspace, cudaStream_t stream) override;
+
+        virtual size_t getSerializationSize() const override;
+
+        virtual void serialize(void* buffer) const override;
+
+        bool supportsFormatCombination(int pos, const PluginTensorDesc* inOut, int nbInputs, int nbOutputs) const override {
+            return inOut[pos].format == TensorFormat::kLINEAR && inOut[pos].type == DataType::kFLOAT;
+        }
+
+        const char* getPluginType() const override;
+
+        const char* getPluginVersion() const override;
+
+        void destroy() override;
+
+        IPluginV2IOExt* clone() const override;
+
+        void setPluginNamespace(const char* pluginNamespace) override;
+
+        const char* getPluginNamespace() const override;
+
+        DataType getOutputDataType(int index, const nvinfer1::DataType* inputTypes, int nbInputs) const override;
+
+        bool isOutputBroadcastAcrossBatch(int outputIndex, const bool* inputIsBroadcasted, int nbInputs) const override;
+
+        bool canBroadcastInputAcrossBatch(int inputIndex) const override;
+
+        void attachToContext(
+            cudnnContext* cudnnContext, cublasContext* cublasContext, IGpuAllocator* gpuAllocator) override;
+
+        void configurePlugin(const PluginTensorDesc* in, int nbInput, const PluginTensorDesc* out, int nbOutput) override;
+
+        void detachFromContext() override;
+
+    private:
+        void forwardGpu(const float *const * inputs, float * output, cudaStream_t stream, int batchSize = 1);
+        int mThreadCount = 256;
+        const char* mPluginNamespace;
+        int mKernelCount;
+        int mClassCount;
+        int mNetWidth;
+        int mNetHeight;
+        int mMaxOutObject;
+        std::vector<Yolo::YoloKernel> mYoloKernel;
+        void** mAnchor;
+    };
+
+    class YoloPluginCreator : public IPluginCreator
+    {
+    public:
+        YoloPluginCreator();
+
+        ~YoloPluginCreator() override = default;
+
+        const char* getPluginName() const override;
+
+        const char* getPluginVersion() const override;
+
+        const PluginFieldCollection* getFieldNames() override;
+
+        IPluginV2IOExt* createPlugin(const char* name, const PluginFieldCollection* fc) override;
+
+        IPluginV2IOExt* deserializePlugin(const char* name, const void* serialData, size_t serialLength) override;
+
+        void setPluginNamespace(const char* libNamespace) override
+        {
+            mNamespace = libNamespace;
+        }
+
+        const char* getPluginNamespace() const override
+        {
+            return mNamespace.c_str();
+        }
+
+    private:
+        std::string mNamespace;
+        static PluginFieldCollection mFC;
+        static std::vector<PluginField> mPluginAttributes;
+    };
+    REGISTER_TENSORRT_PLUGIN(YoloPluginCreator);
+};
+
+#endif
