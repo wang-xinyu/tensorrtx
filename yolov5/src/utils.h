09445154f1806b2b9826299d591085a4a8f5154a
Wang Xinyu <shaywxy@gmail.com>
2023-01-11 10:46:24 +0800

    Update utils.h

--- yolov5/src/utils.h
+++ yolov5/src/utils.h
@@ -10,8 +10,8 @@
 
 static inline cv::Mat preprocess_img(cv::Mat& img, int input_w, int input_h) {
     int w, h, x, y;
-    float r_w = input_w / (img.cols*1.0);
-    float r_h = input_h / (img.rows*1.0);
+    float r_w = input_w / (img.cols * 1.0);
+    float r_h = input_h / (img.rows * 1.0);
     if (r_h > r_w) {
         w = input_w;
         h = r_w * img.rows;
@@ -30,7 +30,7 @@ static inline cv::Mat preprocess_img(cv::Mat& img, int input_w, int input_h) {
     return out;
 }
 
-static inline int read_files_in_dir(const char *p_dir_name, std::vector<std::string> &file_names) {
+static inline int read_files_in_dir(const char* p_dir_name, std::vector<std::string>& file_names) {
     DIR *p_dir = opendir(p_dir_name);
     if (p_dir == nullptr) {
         return -1;
@@ -53,29 +53,29 @@ static inline int read_files_in_dir(const char *p_dir_name, std::vector<std::str
 }
 
 // Function to trim leading and trailing whitespace from a string
-static inline std::string trim_leading_whitespace(const std::string& str){
+static inline std::string trim_leading_whitespace(const std::string& str) {
     size_t first = str.find_first_not_of(' ');
-    if (std::string::npos == first){
+    if (std::string::npos == first) {
         return str;
     }
     size_t last = str.find_last_not_of(' ');
     return str.substr(first, (last - first + 1));
 }
 // Src: https://stackoverflow.com/questions/16605967
-static inline std::string to_string_with_precision(const float a_value, const int n = 2){
+static inline std::string to_string_with_precision(const float a_value, const int n = 2) {
     std::ostringstream out;
     out.precision(n);
     out << std::fixed << a_value;
     return out.str();
 }
 
-static inline int read_labels(const std::string labels_filename, std::unordered_map<int, std::string> &labels_map) {
+static inline int read_labels(const std::string labels_filename, std::unordered_map<int, std::string>& labels_map) {
 
     std::ifstream file(labels_filename);
     // Read each line of the file
     std::string line;
     int index = 0;
-    while (std::getline(file, line)){
+    while (std::getline(file, line)) {
         // Strip the line of any leading or trailing whitespace
         line = trim_leading_whitespace(line);
 
