f334a1aa3843ac7c60a481450ea96b868e4f4852
mantuoluozk <43333969+mantuoluozk@users.noreply.github.com>
2023-01-11 14:05:42 +0800

    yolop TRT8 support (#1202)
    * yolop TRT8 support
    
    * yolop TRT8 support

--- yolop/logging.h
+++ yolop/logging.h
@@ -9,13 +9,20 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include "macros.h"
+
+#if NV_TENSORRT_MAJOR >= 8
+#define TRT_NOEXCEPT noexcept
+#else
+#define TRT_NOEXCEPT
+#endif
 
 using Severity = nvinfer1::ILogger::Severity;
 
 class Logger : public nvinfer1::ILogger
 {
 public:
-    void log(Severity severity, const char* msg) noexcept override
+    void log(Severity severity, const char* msg) TRT_NOEXCEPT override
     {
         if (severity < Severity::kINFO) {
             std::cout << msg << std::endl;
