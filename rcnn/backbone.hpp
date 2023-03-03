f8ab094f13886aee2a34bc5517cc78e8a7707256
nengwp <44516353+nengwp@users.noreply.github.com>
2023-03-03 11:10:04 +0800

    Update backbone.hpp

--- rcnn/backbone.hpp
+++ rcnn/backbone.hpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <map>
 #include <string>
-#include "common.hpp"  
+#include "common.hpp"
 
 /* when stride>1, whether to put stride in the first 1x1 convolution or the bottleneck 3x3 convolution.
 set false when use backbone from torchvision*/
