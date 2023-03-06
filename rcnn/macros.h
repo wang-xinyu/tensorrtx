08c66acc5d23931827fc985c8aa9e0963bdddc0c
nengwp <44516353+nengwp@users.noreply.github.com>
2023-03-06 10:29:10 +0800

    Update macros.h

--- rcnn/macros.h
+++ rcnn/macros.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <NvInfer.h>
+#include <cuda.h>
+
+#if CUDA_VERSION >=11000
+#define CUDA_11
+#endif
 
 #if NV_TENSORRT_MAJOR >= 8
 #define TRT_NOEXCEPT noexcept
 #define TRT_CONST_ENQUEUE const
-#define CUDA_11 // CUDA 11 TensorRT 8
 #else
 #define TRT_NOEXCEPT
 #define TRT_CONST_ENQUEUE
