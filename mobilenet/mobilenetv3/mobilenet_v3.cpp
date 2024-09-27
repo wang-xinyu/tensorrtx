1192bca4c1a91b5e043db1449906fd6081dc3d67
Phoenix <fengzhe8215@foxmail.com>
2024-09-27 15:32:14 +0800

    fix object destruction order in APIToModel function to avoid undefined behavior

--- mobilenet/mobilenetv3/mobilenet_v3.cpp
+++ mobilenet/mobilenetv3/mobilenet_v3.cpp
@@ -384,8 +384,8 @@ void APIToModel(unsigned int maxBatchSize, IHostMemory** modelStream, std::strin
 
     // Close everything down
     engine->destroy();
-    builder->destroy();
     config->destroy();
+    builder->destroy();
 }
 
 void doInference(IExecutionContext& context, float* input, float* output, int batchSize)
