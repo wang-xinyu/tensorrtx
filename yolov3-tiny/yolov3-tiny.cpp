3902cb040297ece1e8bc9f2318100e420fbae981
zhouxiaolong <18375319235@163.com>
2025-11-26 09:45:14 +0800

    format the file.

--- yolov3-tiny/yolov3-tiny.cpp
+++ yolov3-tiny/yolov3-tiny.cpp
@@ -1,25 +1,23 @@
+#include <dirent.h>
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <opencv2/opencv.hpp>
 #include <sstream>
 #include <vector>
-#include <chrono>
-#include <opencv2/opencv.hpp>
-#include <dirent.h>
 #include "NvInfer.h"
 #include "cuda_runtime_api.h"
 #include "logging.h"
 #include "yololayer.h"
 
-#define CHECK(status) \
-    do\
-    {\
-        auto ret = (status);\
-        if (ret != 0)\
-        {\
-            std::cerr << "Cuda failure: " << ret << std::endl;\
-            abort();\
-        }\
+#define CHECK(status)                                          \
+    do {                                                       \
+        auto ret = (status);                                   \
+        if (ret != 0) {                                        \
+            std::cerr << "Cuda failure: " << ret << std::endl; \
+            abort();                                           \
+        }                                                      \
     } while (0)
 
 #define USE_FP16  // comment out this if want to use FP32
@@ -32,22 +30,23 @@ using namespace nvinfer1;
 // stuff we know about the network and the input/output blobs
 static const int INPUT_H = Yolo::INPUT_H;
 static const int INPUT_W = Yolo::INPUT_W;
-static const int OUTPUT_SIZE = 1000 * 7 + 1;  // we assume the yololayer outputs no more than 1000 boxes that conf >= 0.1
+static const int OUTPUT_SIZE =
+        1000 * 7 + 1;  // we assume the yololayer outputs no more than 1000 boxes that conf >= 0.1
 const char* INPUT_BLOB_NAME = "data";
 const char* OUTPUT_BLOB_NAME = "prob";
 static Logger gLogger;
 
 cv::Mat preprocess_img(cv::Mat& img) {
     int w, h, x, y;
-    float r_w = INPUT_W / (img.cols*1.0);
-    float r_h = INPUT_H / (img.rows*1.0);
+    float r_w = INPUT_W / (img.cols * 1.0);
+    float r_h = INPUT_H / (img.rows * 1.0);
     if (r_h > r_w) {
         w = INPUT_W;
         h = r_w * img.rows;
         x = 0;
         y = (INPUT_H - h) / 2;
     } else {
-        w = r_h* img.cols;
+        w = r_h * img.cols;
         h = INPUT_H;
         x = (INPUT_W - w) / 2;
         y = 0;
@@ -64,53 +63,55 @@ cv::Rect get_rect(cv::Mat& img, float bbox[4]) {
     float r_w = INPUT_W / (img.cols * 1.0);
     float r_h = INPUT_H / (img.rows * 1.0);
     if (r_h > r_w) {
-        l = bbox[0] - bbox[2]/2.f;
-        r = bbox[0] + bbox[2]/2.f;
-        t = bbox[1] - bbox[3]/2.f - (INPUT_H - r_w * img.rows) / 2;
-        b = bbox[1] + bbox[3]/2.f - (INPUT_H - r_w * img.rows) / 2;
+        l = bbox[0] - bbox[2] / 2.f;
+        r = bbox[0] + bbox[2] / 2.f;
+        t = bbox[1] - bbox[3] / 2.f - (INPUT_H - r_w * img.rows) / 2;
+        b = bbox[1] + bbox[3] / 2.f - (INPUT_H - r_w * img.rows) / 2;
         l = l / r_w;
         r = r / r_w;
         t = t / r_w;
         b = b / r_w;
     } else {
-        l = bbox[0] - bbox[2]/2.f - (INPUT_W - r_h * img.cols) / 2;
-        r = bbox[0] + bbox[2]/2.f - (INPUT_W - r_h * img.cols) / 2;
-        t = bbox[1] - bbox[3]/2.f;
-        b = bbox[1] + bbox[3]/2.f;
+        l = bbox[0] - bbox[2] / 2.f - (INPUT_W - r_h * img.cols) / 2;
+        r = bbox[0] + bbox[2] / 2.f - (INPUT_W - r_h * img.cols) / 2;
+        t = bbox[1] - bbox[3] / 2.f;
+        b = bbox[1] + bbox[3] / 2.f;
         l = l / r_h;
         r = r / r_h;
         t = t / r_h;
         b = b / r_h;
     }
-    return cv::Rect(l, t, r-l, b-t);
+    return cv::Rect(l, t, r - l, b - t);
 }
 
 float iou(float lbox[4], float rbox[4]) {
     float interBox[] = {
-        std::max(lbox[0] - lbox[2]/2.f , rbox[0] - rbox[2]/2.f), //left
-        std::min(lbox[0] + lbox[2]/2.f , rbox[0] + rbox[2]/2.f), //right
-        std::max(lbox[1] - lbox[3]/2.f , rbox[1] - rbox[3]/2.f), //top
-        std::min(lbox[1] + lbox[3]/2.f , rbox[1] + rbox[3]/2.f), //bottom
+            std::max(lbox[0] - lbox[2] / 2.f, rbox[0] - rbox[2] / 2.f),  //left
+            std::min(lbox[0] + lbox[2] / 2.f, rbox[0] + rbox[2] / 2.f),  //right
+            std::max(lbox[1] - lbox[3] / 2.f, rbox[1] - rbox[3] / 2.f),  //top
+            std::min(lbox[1] + lbox[3] / 2.f, rbox[1] + rbox[3] / 2.f),  //bottom
     };
 
-    if(interBox[2] > interBox[3] || interBox[0] > interBox[1])
+    if (interBox[2] > interBox[3] || interBox[0] > interBox[1])
         return 0.0f;
 
-    float interBoxS =(interBox[1]-interBox[0])*(interBox[3]-interBox[2]);
-    return interBoxS/(lbox[2]*lbox[3] + rbox[2]*rbox[3] -interBoxS);
+    float interBoxS = (interBox[1] - interBox[0]) * (interBox[3] - interBox[2]);
+    return interBoxS / (lbox[2] * lbox[3] + rbox[2] * rbox[3] - interBoxS);
 }
 
 bool cmp(const Yolo::Detection& a, const Yolo::Detection& b) {
     return a.det_confidence > b.det_confidence;
 }
 
-void nms(std::vector<Yolo::Detection>& res, float *output, float nms_thresh = NMS_THRESH) {
+void nms(std::vector<Yolo::Detection>& res, float* output, float nms_thresh = NMS_THRESH) {
     std::map<float, std::vector<Yolo::Detection>> m;
     for (int i = 0; i < output[0] && i < 1000; i++) {
-        if (output[1 + 7 * i + 4] <= BBOX_CONF_THRESH) continue;
+        if (output[1 + 7 * i + 4] <= BBOX_CONF_THRESH)
+            continue;
         Yolo::Detection det;
         memcpy(&det, &output[1 + 7 * i], 7 * sizeof(float));
-        if (m.count(det.class_id) == 0) m.emplace(det.class_id, std::vector<Yolo::Detection>());
+        if (m.count(det.class_id) == 0)
+            m.emplace(det.class_id, std::vector<Yolo::Detection>());
         m[det.class_id].push_back(det);
     }
     for (auto it = m.begin(); it != m.end(); it++) {
@@ -122,7 +123,7 @@ void nms(std::vector<Yolo::Detection>& res, float *output, float nms_thresh = NM
             res.push_back(item);
             for (size_t n = m + 1; n < dets.size(); ++n) {
                 if (iou(item.bbox, dets[n].bbox) > nms_thresh) {
-                    dets.erase(dets.begin()+n);
+                    dets.erase(dets.begin() + n);
                     --n;
                 }
             }
@@ -145,8 +146,7 @@ std::map<std::string, Weights> loadWeights(const std::string file) {
     input >> count;
     assert(count > 0 && "Invalid weight map file.");
 
-    while (count--)
-    {
+    while (count--) {
         Weights wt{DataType::kFLOAT, nullptr, 0};
         uint32_t size;
 
@@ -157,12 +157,11 @@ std::map<std::string, Weights> loadWeights(const std::string file) {
 
         // Load blob
         uint32_t* val = reinterpret_cast<uint32_t*>(malloc(sizeof(val) * size));
-        for (uint32_t x = 0, y = size; x < y; ++x)
-        {
+        for (uint32_t x = 0, y = size; x < y; ++x) {
             input >> std::hex >> val[x];
         }
         wt.values = val;
-        
+
         wt.count = size;
         weightMap[name] = wt;
     }
@@ -170,26 +169,27 @@ std::map<std::string, Weights> loadWeights(const std::string file) {
     return weightMap;
 }
 
-IScaleLayer* addBatchNorm2d(INetworkDefinition *network, std::map<std::string, Weights>& weightMap, ITensor& input, std::string lname, float eps) {
-    float *gamma = (float*)weightMap[lname + ".weight"].values;
-    float *beta = (float*)weightMap[lname + ".bias"].values;
-    float *mean = (float*)weightMap[lname + ".running_mean"].values;
-    float *var = (float*)weightMap[lname + ".running_var"].values;
+IScaleLayer* addBatchNorm2d(INetworkDefinition* network, std::map<std::string, Weights>& weightMap, ITensor& input,
+                            std::string lname, float eps) {
+    float* gamma = (float*)weightMap[lname + ".weight"].values;
+    float* beta = (float*)weightMap[lname + ".bias"].values;
+    float* mean = (float*)weightMap[lname + ".running_mean"].values;
+    float* var = (float*)weightMap[lname + ".running_var"].values;
     int len = weightMap[lname + ".running_var"].count;
 
-    float *scval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
+    float* scval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
     for (int i = 0; i < len; i++) {
         scval[i] = gamma[i] / sqrt(var[i] + eps);
     }
     Weights scale{DataType::kFLOAT, scval, len};
-    
-    float *shval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
+
+    float* shval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
     for (int i = 0; i < len; i++) {
         shval[i] = beta[i] - mean[i] * gamma[i] / sqrt(var[i] + eps);
     }
     Weights shift{DataType::kFLOAT, shval, len};
 
-    float *pval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
+    float* pval = reinterpret_cast<float*>(malloc(sizeof(float) * len));
     for (int i = 0; i < len; i++) {
         pval[i] = 1.0;
     }
@@ -203,14 +203,18 @@ IScaleLayer* addBatchNorm2d(INetworkDefinition *network, std::map<std::string, W
     return scale_1;
 }
 
-ILayer* convBnLeaky(INetworkDefinition *network, std::map<std::string, Weights>& weightMap, ITensor& input,  int outch, int ksize, int s, int p, int linx) {
+ILayer* convBnLeaky(INetworkDefinition* network, std::map<std::string, Weights>& weightMap, ITensor& input, int outch,
+                    int ksize, int s, int p, int linx) {
     Weights emptywts{DataType::kFLOAT, nullptr, 0};
-    IConvolutionLayer* conv1 = network->addConvolutionNd(input, outch, DimsHW{ksize, ksize}, weightMap["module_list." + std::to_string(linx) + ".Conv2d.weight"], emptywts);
+    IConvolutionLayer* conv1 =
+            network->addConvolutionNd(input, outch, DimsHW{ksize, ksize},
+                                      weightMap["module_list." + std::to_string(linx) + ".Conv2d.weight"], emptywts);
     assert(conv1);
     conv1->setStrideNd(DimsHW{s, s});
     conv1->setPaddingNd(DimsHW{p, p});
 
-    IScaleLayer* bn1 = addBatchNorm2d(network, weightMap, *conv1->getOutput(0), "module_list." + std::to_string(linx) + ".BatchNorm2d", 1e-4);
+    IScaleLayer* bn1 = addBatchNorm2d(network, weightMap, *conv1->getOutput(0),
+                                      "module_list." + std::to_string(linx) + ".BatchNorm2d", 1e-4);
 
     auto lr = network->addActivation(*bn1->getOutput(0), ActivationType::kLEAKY_RELU);
     lr->setAlpha(0.1);
@@ -251,17 +255,20 @@ ICudaEngine* createEngine(unsigned int maxBatchSize, IBuilder* builder, IBuilder
     auto lr12 = convBnLeaky(network, weightMap, *pool11->getOutput(0), 1024, 3, 1, 1, 12);
     auto lr13 = convBnLeaky(network, weightMap, *lr12->getOutput(0), 256, 1, 1, 0, 13);
     auto lr14 = convBnLeaky(network, weightMap, *lr13->getOutput(0), 512, 3, 1, 1, 14);
-    IConvolutionLayer* conv15 = network->addConvolutionNd(*lr14->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["module_list.15.Conv2d.weight"], weightMap["module_list.15.Conv2d.bias"]);
+    IConvolutionLayer* conv15 = network->addConvolutionNd(*lr14->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1},
+                                                          weightMap["module_list.15.Conv2d.weight"],
+                                                          weightMap["module_list.15.Conv2d.bias"]);
     // 16 is yolo
     auto l17 = lr13;
     auto lr18 = convBnLeaky(network, weightMap, *l17->getOutput(0), 128, 1, 1, 0, 18);
 
-    float *deval = reinterpret_cast<float*>(malloc(sizeof(float) * 128 * 2 * 2));
+    float* deval = reinterpret_cast<float*>(malloc(sizeof(float) * 128 * 2 * 2));
     for (int i = 0; i < 128 * 2 * 2; i++) {
         deval[i] = 1.0;
     }
     Weights deconvwts19{DataType::kFLOAT, deval, 128 * 2 * 2};
-    IDeconvolutionLayer* deconv19 = network->addDeconvolutionNd(*lr18->getOutput(0), 128, DimsHW{2, 2}, deconvwts19, emptywts);
+    IDeconvolutionLayer* deconv19 =
+            network->addDeconvolutionNd(*lr18->getOutput(0), 128, DimsHW{2, 2}, deconvwts19, emptywts);
     assert(deconv19);
     deconv19->setStrideNd(DimsHW{2, 2});
     deconv19->setNbGroups(128);
@@ -270,12 +277,14 @@ ICudaEngine* createEngine(unsigned int maxBatchSize, IBuilder* builder, IBuilder
     ITensor* inputTensors[] = {deconv19->getOutput(0), lr8->getOutput(0)};
     auto cat20 = network->addConcatenation(inputTensors, 2);
     auto lr21 = convBnLeaky(network, weightMap, *cat20->getOutput(0), 256, 3, 1, 1, 21);
-    IConvolutionLayer* conv22 = network->addConvolutionNd(*lr21->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1}, weightMap["module_list.22.Conv2d.weight"], weightMap["module_list.22.Conv2d.bias"]);
+    IConvolutionLayer* conv22 = network->addConvolutionNd(*lr21->getOutput(0), 3 * (Yolo::CLASS_NUM + 5), DimsHW{1, 1},
+                                                          weightMap["module_list.22.Conv2d.weight"],
+                                                          weightMap["module_list.22.Conv2d.bias"]);
     // 22 is yolo
 
     auto creator = getPluginRegistry()->getPluginCreator("YoloLayer_TRT", "1");
     const PluginFieldCollection* pluginData = creator->getFieldNames();
-    IPluginV2 *pluginObj = creator->createPlugin("yololayer", pluginData);
+    IPluginV2* pluginObj = creator->createPlugin("yololayer", pluginData);
     ITensor* inputTensors_yolo[] = {conv15->getOutput(0), conv22->getOutput(0)};
     auto yolo = network->addPluginV2(inputTensors_yolo, 2, *pluginObj);
 
@@ -291,9 +300,8 @@ ICudaEngine* createEngine(unsigned int maxBatchSize, IBuilder* builder, IBuilder
     std::cout << "Build engine successfully!" << std::endl;
 
     // Release host memory
-    for (auto& mem : weightMap)
-    {
-        free((void*) (mem.second.values));
+    for (auto& mem : weightMap) {
+        free((void*)(mem.second.values));
     }
 
     return engine;
@@ -319,11 +327,11 @@ void doInference(IExecutionContext& context, float* input, float* output, int ba
     // Engine requires exactly IEngine::getNbBindings() number of buffers.
     assert(engine.getNbIOTensors() == 2);
 
-    const char * inputName = INPUT_BLOB_NAME;
-    const char * outputName = OUTPUT_BLOB_NAME;
+    const char* inputName = INPUT_BLOB_NAME;
+    const char* outputName = OUTPUT_BLOB_NAME;
 
-    void * deviceInput{nullptr};
-    void * deviceOutput{nullptr};
+    void* deviceInput{nullptr};
+    void* deviceOutput{nullptr};
 
     // Create GPU buffers on device
     CHECK(cudaMalloc(&deviceInput, batchSize * 3 * INPUT_H * INPUT_W * sizeof(float)));
@@ -334,14 +342,16 @@ void doInference(IExecutionContext& context, float* input, float* output, int ba
     CHECK(cudaStreamCreate(&stream));
 
     // DMA input batch data to device, infer on the batch asynchronously, and DMA output back to host
-    CHECK(cudaMemcpyAsync(deviceInput, input, batchSize * 3 * INPUT_H * INPUT_W * sizeof(float), cudaMemcpyHostToDevice, stream));
-    
+    CHECK(cudaMemcpyAsync(deviceInput, input, batchSize * 3 * INPUT_H * INPUT_W * sizeof(float), cudaMemcpyHostToDevice,
+                          stream));
+
     context.setTensorAddress(inputName, deviceInput);
     context.setTensorAddress(outputName, deviceOutput);
 
     context.enqueueV3(stream);
 
-    CHECK(cudaMemcpyAsync(output, deviceOutput, batchSize * OUTPUT_SIZE * sizeof(float), cudaMemcpyDeviceToHost, stream));
+    CHECK(cudaMemcpyAsync(output, deviceOutput, batchSize * OUTPUT_SIZE * sizeof(float), cudaMemcpyDeviceToHost,
+                          stream));
     cudaStreamSynchronize(stream);
 
     // Release stream and buffers
@@ -350,16 +360,15 @@ void doInference(IExecutionContext& context, float* input, float* output, int ba
     CHECK(cudaFree(deviceOutput));
 }
 
-int read_files_in_dir(const char *p_dir_name, std::vector<std::string> &file_names) {
-    DIR *p_dir = opendir(p_dir_name);
+int read_files_in_dir(const char* p_dir_name, std::vector<std::string>& file_names) {
+    DIR* p_dir = opendir(p_dir_name);
     if (p_dir == nullptr) {
         return -1;
     }
 
     struct dirent* p_file = nullptr;
     while ((p_file = readdir(p_dir)) != nullptr) {
-        if (strcmp(p_file->d_name, ".") != 0 &&
-                strcmp(p_file->d_name, "..") != 0) {
+        if (strcmp(p_file->d_name, ".") != 0 && strcmp(p_file->d_name, "..") != 0) {
             //std::string cur_file_name(p_dir_name);
             //cur_file_name += "/";
             //cur_file_name += p_file->d_name;
@@ -375,7 +384,7 @@ int read_files_in_dir(const char *p_dir_name, std::vector<std::string> &file_nam
 int main(int argc, char** argv) {
     cudaSetDevice(DEVICE);
     // create a model using the API directly and serialize it to a stream
-    char *trtModelStream{nullptr};
+    char* trtModelStream{nullptr};
     size_t size{0};
 
     if (argc == 2 && std::string(argv[1]) == "-s") {
@@ -427,11 +436,12 @@ int main(int argc, char** argv) {
     delete[] trtModelStream;
 
     int fcount = 0;
-    for (auto f: file_names) {
+    for (auto f : file_names) {
         fcount++;
         std::cout << fcount << "  " << f << std::endl;
         cv::Mat img = cv::imread(std::string(argv[2]) + "/" + f);
-        if (img.empty()) continue;
+        if (img.empty())
+            continue;
         cv::Mat pr_img = preprocess_img(img);
         for (int i = 0; i < INPUT_H * INPUT_W; i++) {
             data[i] = pr_img.at<cv::Vec3b>(i)[2] / 255.0;
@@ -446,19 +456,20 @@ int main(int argc, char** argv) {
         std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
         std::vector<Yolo::Detection> res;
         nms(res, prob);
-        for (int i=0; i<20; i++) {
+        for (int i = 0; i < 20; i++) {
             std::cout << prob[i] << ",";
         }
         std::cout << res.size() << std::endl;
         for (size_t j = 0; j < res.size(); j++) {
-            float *p = (float*)&res[j];
+            float* p = (float*)&res[j];
             for (size_t k = 0; k < 7; k++) {
                 std::cout << p[k] << ", ";
             }
             std::cout << std::endl;
             cv::Rect r = get_rect(img, res[j].bbox);
             cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
-            cv::putText(img, std::to_string((int)res[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+            cv::putText(img, std::to_string((int)res[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2,
+                        cv::Scalar(0xFF, 0xFF, 0xFF), 2);
         }
         cv::imwrite("_" + f, img);
     }
