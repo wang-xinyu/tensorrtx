dbc4b9434c608ee94a300f0d6eb02b747703289c
ydk61 <18270880767@163.com>
2025-12-15 20:16:28 +0800

    TensorRT inference yolov13

---
+++ yolov13/include/model.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "NvInfer.h"
+#include <assert.h>
+#include <string>
+
+nvinfer1::IHostMemory *
+buildEngineYolov13Det(nvinfer1::IBuilder *builder,
+                      nvinfer1::IBuilderConfig *config, nvinfer1::DataType dt,
+                      const std::string &wts_path, float &gd, float &gw,
+                      int &max_channels, std::string &type);
+
+nvinfer1::IHostMemory *buildEngineYolov13Det_debug(
+    nvinfer1::IBuilder *builder, nvinfer1::IBuilderConfig *config,
+    nvinfer1::DataType dt, const std::string &wts_path, float &gd, float &gw,
+    int &max_channels, std::string &type);
